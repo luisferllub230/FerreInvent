95225b832fabc43aae04db7dedae52fa3624ccc4
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2023-03-25 19:27:34 -0400

    entities created

---
+++ FerreInvetAPI/Source.Core.Domain/Entities/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source.Core.Domain.Entities
+{
+    public class Inventory
+    {
+        public int id { get; set; }
+        public string inventoryName { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public double discount { get; set; }
+        public string bran { get; set; }
+
+        //Sales
+        public ICollection<Sales> sales { get; set; }
+        
+        //Category
+        public int categoryID { get; set; }
+        public Categories categories { get; set; }
+    }
+}
---
+++ FerreInvetAPI/Source.Core.Domain/Entities/Sales.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source.Core.Domain.Entities
+{
+    public class Sales
+    {
+        public int id { get; set; }
+        public DateTime dateSales { get; set; }
+        public int quantity { get; set; }
+        public float totalCosto { get; set; } 
+        
+        //CUSTUMER
+        public int custumerID { get; set; }
+        public Custumers custumer { get; set; }
+
+        //INVENTORY
+        public int inventoryID { get; set; }
+        public Inventory inventory { get; set; }
+    }
+}
